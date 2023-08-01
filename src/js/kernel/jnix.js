b7bc3b5b85ea1a69cc717f63d01f3907357c5f33
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-08-01 10:07:10 +1000

    starting over again!
    with more research, the project had some major flaws from the start that should've been properly explored before getting too deep into it. Planning to start by addressing the foundations before getting carried away!

--- src/js/kernel/jnix.js
+++
@@ -1,9 +0,0 @@
-import { scheduler } from '/js/kernel/scheduler.js';
-import { moduleLoader } from '/js/kernel/module_loader.js';
-
-moduleLoader.load('textdrawer');
-
-setTimeout(() => { moduleLoader.unload('textdrawer'); }, 2000);
-
-scheduler.process_start( '/js/lib/programs/test.js', 'test', 1, 0, 0, {}, [] );
-scheduler.process_queue_allocate();
--- src/js/lib/sharedlib.js
+++
@@ -1,5 +0,0 @@
-/**
- * Shared library between Processes and the Kernel.
- * 
- * Contains code that is useful between them.
- */
