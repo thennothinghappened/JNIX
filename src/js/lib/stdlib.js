2e0552011ee2e596ca80621429a31037f38137e3
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-08-03 11:35:17 +1000

    remove compiler testing files

--- src/js/lib/stdlib.js
+++
@@ -1,6 +0,0 @@
-import { write } from '/js/lib/fs.js';
-///#endimports
-
-export function say_hi() {
-    write( 0, 'hi!' );
-}
--- src/js/lib/test_project/main.js
+++
@@ -1,6 +0,0 @@
-import { do_stuff } from '/js/lib/test_project/testlib.js';
-import { write } from '/js/lib/fs.js';
-///#endimports
-
-do_stuff();
-write(0, 'hi from main :)');
